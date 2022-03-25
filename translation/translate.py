85ade83a31877f245088b2004038ba6552694350
Rasmus Rynell <rynell.rasmus@gmail.com>
2022-03-25 17:50:54 +0100

    update

--- translation/translate.py
+++ translation/translate.py
@@ -1,9 +1,11 @@
 import json
+from unidecode import unidecode
 
 with open('./translation/names.txt', 'r') as f:
     team_names = json.load(f)
     # Set all team names and their translations to lowercase
-    team_names = {k.lower(): v.lower() for k, v in team_names.items()}
+    team_names = {unidecode(k.lower()): unidecode(v.lower()) for k, v in team_names.items()}
 
 def translate_team_name(name):
+    name = unidecode(name)
     return team_names[name.lower()] if name.lower() in team_names else name
